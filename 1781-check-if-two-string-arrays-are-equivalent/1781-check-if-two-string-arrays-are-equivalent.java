3e2d687badeabf160247641a9cdd06ab446b121c
PARASURAMAN P <velpazhani049@gmail.com>
2025-04-08 20:30:19 +0530

    Time: 1 ms (59.4%), Space: 41.6 MB (50.27%) - LeetHub

--- 1781-check-if-two-string-arrays-are-equivalent/1781-check-if-two-string-arrays-are-equivalent.java
+++ 1781-check-if-two-string-arrays-are-equivalent/1781-check-if-two-string-arrays-are-equivalent.java
@@ -4,10 +4,6 @@ class Solution {
         String w2 ="";
         for(String wo1 : word1) w1=w1+wo1;
         for(String wo2 : word2) w2=w2+wo2;
-        if(w1.length() !=w2.length()) return false;   
-        for(int i=0;i<w1.length();i++){
-            if(w1.charAt(i) != w2.charAt(i)) return false;
-        }
-        return true;
+        return w1.equals(w2);
     }
 }
