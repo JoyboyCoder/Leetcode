000d46300b758b94c9ff830614973e3c3f9f4105
PARASURAMAN P <velpazhani049@gmail.com>
2025-04-08 23:35:20 +0530

    Time: 0 ms (100%), Space: 41.7 MB (81.23%) - LeetHub

--- 1944-truncate-sentence/1944-truncate-sentence.java
+++ 1944-truncate-sentence/1944-truncate-sentence.java
@@ -1,7 +1,15 @@
 class Solution {
     public String truncateSentence(String s, int k) {
-        String[] word=s.split(" ");
-        String[] ans=Arrays.copyOfRange(word,0,k);
-        return String.join(" ",ans);
+        int spaceCount = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                spaceCount++;
+                if (spaceCount == k) {
+                    return s.substring(0, i); 
+                }
+            }
+        }
+        return s;
     }
 }
