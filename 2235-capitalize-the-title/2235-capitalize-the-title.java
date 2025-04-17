6e36eb6df0e60ed023a6e8217306728130373242
PARASURAMAN P <velpazhani049@gmail.com>
2025-04-17 12:43:18 +0530

    Time: 1 ms (99.12%), Space: 42.2 MB (92.83%) - LeetHub

--- 2235-capitalize-the-title/2235-capitalize-the-title.java
+++ 2235-capitalize-the-title/2235-capitalize-the-title.java
@@ -1,19 +1,18 @@
 class Solution {
-    public String capitalizeTitle(String title) {
-        char[] ch = title.toCharArray();
-        for(int i = 0;i<ch.length;i++){
-            int firstInd = i;
+	public String capitalizeTitle(String title) {
+		char[] ch = title.toCharArray();
+		int len = ch.length;
+		for(int i = 0; i < len; ++i) {
+			int firstIndex = i; 
+			while(i < len && ch[i] != ' ') {
+				ch[i] = Character.toLowerCase(ch[i]);
+				++i;
+			}
+			if(i - firstIndex > 2) {
+				ch[firstIndex] =  Character.toUpperCase(ch[firstIndex]); 
+			}
+		}
 
-            while(i < ch.length && ch[i] != ' '){
-                ch[i] |= 32;
-                i++;
-            }
-
-            if(i - firstInd > 2){
-                ch[firstInd] &= ~32;
-            }
-        }
-
-        return String.valueOf(ch);
-    }
+		return String.valueOf(ch);
+}
 }
