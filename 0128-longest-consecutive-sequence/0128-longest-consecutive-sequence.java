1627a6160afabeb937107a9919829c502f306086
PARASURAMAN P <velpazhani049@gmail.com>
2025-08-21 20:02:34 +0530

    Time: 340 ms (5.09%), Space: 67.8 MB (28.8%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,62 +1,31 @@
 class Solution {
-    static 
-    {
-        for(int i=0;i<500;i++)
-            longestConsecutive(new int[]{});
-    }
-    public static int longestConsecutive(int[] arr) 
-    {
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        int cnt=0;
-        int longest =0;
-        for(int a : arr)
-        {
-            max = Math.max(max,a);
-            min = Math.min(min,a);
-        }
-        if(max<100000)
-        {
-            boolean b[] = new boolean[max-min+1];
-            for(int i : arr)
-            {
-                b[i-min]=true;
-            }
-            for(boolean val :b  )
-            {
-                if(val)
-                    cnt++;
-                else
-                {   
-                    longest =Math.max(cnt,longest);
-                    cnt=0;
-                } 
-            }
-             longest =Math.max(cnt,longest);
-        }
-        else
-        {
-        Set<Integer> ele = new HashSet<>();
-        for(int i : arr)
-        {
-            ele.add(i);
-        }
-        for(int i : ele)
-        {
-            if(!ele.contains(i-1))
-            {
-                int temp = i;
-                while(ele.contains(temp))
-                {
-                    cnt++;
-                    temp++;
-                }
-                longest=Math.max(cnt,longest);
-                cnt=0;
-            }
-        }
+    public int longestConsecutive(int[] a) {
+        int n = a.length;
+        if (n == 0)
+            return 0;
+
+        int longest = 1;
+        Set<Integer> set = new HashSet<>();
+
+        // put all the array elements into set
+        for (int i = 0; i < n; i++) {
+            set.add(a[i]);
         }
 
+        // Find the longest sequence
+        for (int it : set) {
+            // if 'it' is a starting number
+            if (!set.contains(it - 1)) {
+                // find consecutive numbers
+                int cnt = 1;
+                int x = it;
+                while (set.contains(x + 1)) {
+                    x = x + 1;
+                    cnt = cnt + 1;
+                }
+                longest = Math.max(longest, cnt);
+            }
+        }
         return longest;
     }
 }
