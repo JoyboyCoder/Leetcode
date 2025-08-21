82d99e23eb8cc2209571e6781374e2356af9e61d
PARASURAMAN P <velpazhani049@gmail.com>
2025-08-21 12:19:01 +0530

    Time: 524 ms (6.4%), Space: 76.9 MB (76.57%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,18 +1,46 @@
+// class Solution {
+//     public int[] rearrangeArray(int[] nums) {
+//         int [] ar =new int[nums.length];
+//         int positive = 0;
+//         int negative = 1;
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i] > 0){
+
+//                 ar[positive] = nums[i];
+//                 positive += 2;
+//             } else {
+//                 ar[negative] = nums[i];
+//                 negative += 2;
+//             }
+//         }
+//         return ar ;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int ans[] = new int[nums.length];
-        int pos=0;
-        int neg =1;
-        for(int num:nums){
-            if(num<0){
-                ans[neg]=num;
-                neg+=2;
-            }
-            else{
-                ans[pos]=num;
-                pos+=2;
+        int [] l = new int[nums.length];
+        int positive = 0;
+        int negative = 1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] > 0){
+                l[positive] = nums[i];
+                positive += 2;
+            } else {
+                l[negative] = nums[i];
+                negative += 2;
             }
         }
-        return ans;
+        return l;
     }
 }
+        
