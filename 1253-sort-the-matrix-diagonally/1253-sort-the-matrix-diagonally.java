b7d7e4a07058a6c64c5a95d4d0d668b4f87fa601
PARASURAMAN P <velpazhani049@gmail.com>
2025-09-21 13:23:04 +0530

    Time: 5 ms (84.13%), Space: 45 MB (60.88%) - LeetHub

--- 1253-sort-the-matrix-diagonally/1253-sort-the-matrix-diagonally.java
+++ 1253-sort-the-matrix-diagonally/1253-sort-the-matrix-diagonally.java
@@ -1,16 +1,35 @@
 class Solution {
-     public int[][] diagonalSort(int[][] A) {
-        int m = A.length, n = A[0].length;
-        HashMap<Integer, PriorityQueue<Integer>> d = new HashMap<>();
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                d.putIfAbsent(i - j, new PriorityQueue<>());
-                d.get(i - j).add(A[i][j]);
-            }
+    public int[][] diagonalSort(int[][] mat) {
+        int rows = mat.length;
+        int cols = mat[0].length;
+
+        for (int j = 0; j < cols; j++) sortDiagonal(mat, 0, j);
+        for (int i = 1; i < rows; i++) sortDiagonal(mat, i, 0);
+
+        return mat;
+    }
+
+    private void sortDiagonal(int[][] mat, int row, int col) {
+        int rows = mat.length;
+        int cols = mat[0].length;
+        List<Integer> diagonal = new ArrayList<>();
+
+        int i = row, j = col;
+        while (i < rows && j < cols) {
+            diagonal.add(mat[i][j]);
+            i++;
+            j++;
         }
-        for (int i = 0; i < m; ++i)
-            for (int j = 0; j < n; ++j)
-                A[i][j] = d.get(i - j).poll();
-        return A;
+
+        Collections.sort(diagonal);
+
+        i = row;
+        j = col;
+        int idx = 0;
+        while (i < rows && j < cols) {
+            mat[i][j] = diagonal.get(idx++);
+            i++;
+            j++;
+        }
     }
 }
