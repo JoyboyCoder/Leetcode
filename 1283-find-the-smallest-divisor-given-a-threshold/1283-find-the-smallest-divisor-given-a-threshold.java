0816df6bf319a680ac0d57f1af72f863d43b8bf9
PARASURAMAN P <velpazhani049@gmail.com>
2025-12-11 09:34:45 +0530

    Time: 6 ms (98.21%), Space: 51.2 MB (14.17%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
@@ -1,32 +1,32 @@
 class Solution {
-    static boolean checkDivisor(int[] arr,int divisor,int th){
-        int sum=0;
-        for(int i=0;i<arr.length;i++){
-            sum+= (arr[i]+divisor-1)/divisor;
-        }
-        if (sum<=th) return true;
-        else return false;
-    }
     public int smallestDivisor(int[] nums, int threshold) {
-
-        int left=1;
-        int right=0;
-        for(int x:nums){
-            right=Math.max(x,right);
-        }
-
-        int ans=-1;
-        while(left<=right){
-            int mid=(left+right)/2;
-
-            if(checkDivisor(nums,mid,threshold)){
-                ans=mid;
-                right=mid-1;
-            }else{
-                left=mid+1;
+        int maxNum = 0;
+        for (int num : nums) {
+            if (num > maxNum) {
+                maxNum = num;
             }
         }
-
-        return ans;
+        
+        int low = 1;
+        int high = maxNum;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int sum = 0;
+            for (int num : nums) {
+                sum += (num + mid - 1) / mid;
+                if (sum > threshold) {
+                    break;
+                }
+            }
+            
+            if (sum <= threshold) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        return low;
     }
 }
