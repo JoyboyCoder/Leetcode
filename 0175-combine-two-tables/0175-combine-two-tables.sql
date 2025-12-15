a295b3b7e7c87814486b3e8a15a84115868eb135
PARASURAMAN P <velpazhani049@gmail.com>
2025-12-15 08:48:07 +0530

    Time: 414 ms (27.92%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,6 +1,4 @@
 # Write your MySQL query statement below
 Select p.firstName, p.lastName, a.city, a.state 
 from Person p
-left join address a on p.personId = a.personid
-
-;
+left join address a on p.personId = a.personid;
