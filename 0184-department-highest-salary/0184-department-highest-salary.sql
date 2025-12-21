abbab299714a1658f81d7df193504a953cee5450
PARASURAMAN P <velpazhani049@gmail.com>
2025-12-21 09:02:31 +0530

    Time: 566 ms (92.29%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,11 +1,10 @@
-/* Write your PL/SQL query statement below */
-SELECT d.name AS Department, 
-       e.name AS Employee, 
-       e.salary AS Salary
-FROM Employee e
-JOIN Department d ON e.departmentId = d.id
-WHERE e.salary = (
-    SELECT MAX(salary)
-    FROM Employee
-    WHERE departmentId = e.departmentId
-);
+# Write your MySQL query statement below
+select d.name as Department , e.name as Employee ,e.salary as Salary 
+from Employee e
+INNER JOIN Department d on e.departmentID = d.id
+where 
+(e.DepartmentId,e.Salary)
+ in (
+    select 
+    departmentId , Max(salary) from Employee 
+Group by departmentId);
